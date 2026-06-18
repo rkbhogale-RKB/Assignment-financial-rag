169cf9efcad34b8242a399e2a78179f06e7b960e
rkbhogale-RKB <rkbhogale@gmail.com>
2026-06-18 10:03:03 +0000

    Rerank implementation

--- app/rag.py
+++ app/rag.py
@@ -2,13 +2,16 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
-
+from sentence_transformers import CrossEncoder
 
 print("Loading model")
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
 #chroma db 
 vector_store = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
+
+reranker = CrossEncoder( "cross-encoder/ms-marco-MiniLM-L-6-v2")
+
 
 def process_and_store_document(file_path: str, document_id: int):
    
@@ -31,14 +34,26 @@ def process_and_store_document(file_path: str, document_id: int):
 def search_documents(query: str, top_k: int = 20):
     
     results = vector_store.similarity_search(query, k=top_k)
-    
-    
-    formatted_results = []
+    if not results:
+        return []
+    pairs=[]
+    print(results)
     for doc in results:
+        print(doc)
+        pairs.append((query, doc.page_content))
+    scores = reranker.predict(pairs)
+    scored_results = list(zip(results, scores))
+    scored_results.sort(key=lambda x: x[1], reverse=True)
+
+
+    formatted_results = []
+    for doc, score in scored_results[:5]:
         formatted_results.append({
             "content": doc.page_content,
-            "document_id": doc.metadata.get("document_id", "Unknown")
+            "document_id": doc.metadata.get("document_id", "Unknown"),
+            "rerank_score": float(score) 
         })
+
         
     return formatted_results
 
